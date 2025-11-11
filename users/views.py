31e913a22a5f7349693351ff20cb448b4ebcdadf
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-11-11 16:02:23 -0300

    Add login/logout + home.html

--- users/views.py
+++ users/views.py
@@ -1,13 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from users.forms import CustomUserCreationForm
-
-
-def login_view(request):
-    # GET → renderiza pages/index.html (form de login)
-    if request.method == "GET":
-        return render(request, "pages/index.html")
 
 
 def create_account(request):
@@ -29,3 +25,36 @@ def create_account(request):
         # Se houver erros, mostra a mesma página com mensagens
         messages.error(request, "Corrija os erros abaixo.")
         return render(request, "pages/create-account.html", {"form": form})
+
+
+# Redireciona para o login se não estiver autenticado
+@login_required(login_url="/")
+def home_view(request):
+    return render(request, "pages/home.html")
+
+
+def login_view(request):
+    # Se o usuário já estiver logado, envia direto pra home
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    # GET → renderiza pages/index.html (form de login)
+    if request.method == "GET":
+        return render(request, "pages/index.html")
+
+    # POST → processa credenciais
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect("home")
+    else:
+        messages.error(request, "Usuário ou senha inválidos.")
+        return render(request, "pages/index.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
